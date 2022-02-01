3bf90aff275cdfd850c49f57077ebdcf8236f5a9
Jaya435 <thomasjaya@icloud.com>
2022-02-01 08:32:05 +0000

    added test for reading in ad group data

--- roas_app/ad_campaign/tests/conftest.py
+++ roas_app/ad_campaign/tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 import os
-from ad_campaign.models import Campaigns, SearchTerms
+from ad_campaign.models import AdGroups, Campaigns, SearchTerms
 from django.core.management import call_command
 
 
@@ -19,17 +19,17 @@ def search_terms_filepath(test_directory):
     return os.path.join(test_directory, 'test_search_terms.csv')
 
 
-@pytest.fixture(autouse=True, scope="session")
-def load_campaign_data(django_db_setup, django_db_blocker, campaign_filepath):
-    print(campaign_filepath)
-    with django_db_blocker.unblock():
-        call_command("load_data", campaign_filepath)
+@pytest.fixture(scope='session')
+def ad_groups_filepath(test_directory):
+    return os.path.join(test_directory, 'test_adgroups.csv')
 
 
 @pytest.fixture(autouse=True, scope="session")
-def load_search_terms_data(django_db_setup, django_db_blocker, campaign_filepath, search_terms_filepath):
+def load_data(django_db_setup, django_db_blocker, campaign_filepath, search_terms_filepath, ad_groups_filepath):
     with django_db_blocker.unblock():
-        call_command("load_data", campaign_filepath, "--search_terms", search_terms_filepath)
+        call_command("load_data", campaign_filepath,
+                     "--search_terms", search_terms_filepath,
+                     "--ad_groups", ad_groups_filepath)
 
 
 @pytest.fixture
@@ -40,3 +40,8 @@ def get_all_campaigns(db) -> Campaigns:
 @pytest.fixture
 def get_all_search_terms(db) -> SearchTerms:
     return SearchTerms.objects.all()
+
+
+@pytest.fixture
+def get_all_ad_groups(db) -> AdGroups:
+    return AdGroups.objects.all()
--- roas_app/ad_campaign/tests/test_load_data.py
+++ roas_app/ad_campaign/tests/test_load_data.py
@@ -4,7 +4,7 @@ from django.core.management import call_command
 class TestLoadCampaignData:
 
     def test_load_campaign_data__all_data(self, get_all_campaigns):
-        assert len(get_all_campaigns) == 20
+        assert len(get_all_campaigns) == 32
 
     def test_load_campaign_data__CSV_already_loaded(self, db, campaign_filepath, capsys):
         msg = f"{campaign_filepath} is already saved in the ad_campaign_campaigns table.\n"
@@ -17,8 +17,7 @@ class TestLoadSearchTermsData:
     def test_load_search_terms__all_data(self, get_all_search_terms):
         assert len(get_all_search_terms) == 96
 
-    def test_load_search_terms__CSV_already_loaded(self, db, campaign_filepath, search_terms_filepath, capsys):
-        msg = f"Successfully inserted 96 rows into ad_campaign_searchterms table"
-        call_command("load_data", campaign_filepath, "--search_terms", search_terms_filepath)
-        out, _ = capsys.readouterr()
-        assert msg in out
+
+class TestLoadAdGroupsData:
+    def test_load_ad_groups__all_data(self, get_all_ad_groups):
+        assert len(get_all_ad_groups) == 56
